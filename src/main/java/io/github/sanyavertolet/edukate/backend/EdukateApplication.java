19828c97280538f62429d36739aaefd888b1f88c
sanyavertolet <lxnfrolov@gmail.com>
2025-02-04 23:03:40 +0400

    Initial commit

---
+++ src/main/java/io/github/sanyavertolet/edukate/backend/EdukateApplication.java
@@ -0,0 +1,13 @@
+package io.github.sanyavertolet.edukate.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdukateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdukateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/sanyavertolet/edukate/backend/EdukateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sanyavertolet.edukate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdukateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
