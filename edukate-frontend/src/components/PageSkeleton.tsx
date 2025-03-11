aceba7c8c3f9d93aaa99a940e1bccc65a166eafa
sanyavertolet <lxnfrolov@gmail.com>
2025-03-11 20:35:43 +0400

    Add PendingStatusSnackbar for user approval status
    ### What's done:
     * Integrated `PendingStatusSnackbar` into `PageSkeleton` to notify users with "PENDING" status.
     * Created new `PendingStatusSnackbar` component to display a message about restricted features for pending accounts.

--- edukate-frontend/src/components/PageSkeleton.tsx
+++ edukate-frontend/src/components/PageSkeleton.tsx
@@ -2,10 +2,15 @@ import { Container } from "@mui/material";
 import { TopBar } from "./topbar/TopBar";
 import { Outlet } from "react-router-dom";
 import { ParticlesComponent } from "./animation/Particles";
+import { useAuthContext } from "./auth/AuthContextProvider";
+import { PendingStatusSnackbar } from "./snackbars/PendingStatusSnackbar.tsx";
 
 export default function PageSkeleton() {
+    const { user } = useAuthContext();
+
     return (
         <Container>
+            <PendingStatusSnackbar open={user != undefined && user.status == "PENDING"}/>
             <TopBar/>
             <Container>
                 <Outlet/>
