4f5a0cacf9d18ebed73c529a2d73b399839d522a
sanyavertolet <lxnfrolov@gmail.com>
2025-04-05 20:52:10 +0400

    Add 'Create bundle' button to BundleListView
    ### What's done:
     * Added a 'Create bundle' button with navigation to '/bundles/new'
     * Imported necessary components: Button, Container, AddIcon, and useNavigate
     * Updated the layout to accommodate the new button using MUI styling

--- edukate-frontend/src/views/BundleListView.tsx
+++ edukate-frontend/src/views/BundleListView.tsx
@@ -1,13 +1,32 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import { BundleListComponent } from "../components/bundle/BundleListComponent";
+import { useNavigate } from "react-router-dom";
+import AddIcon from '@mui/icons-material/Add';
 
 export default function BundleListView() {
+    const navigate = useNavigate();
     return (
         <Box>
-            <Typography color="primary" variant="h5">
-                Problem bundles
-            </Typography>
+            <Container>
+                <Box sx={{
+                    display: "flex", justifyContent: "center", alignItems: "center", position: "relative"
+                }}>
+                    <Typography color="primary" variant="h5">
+                        Problem bundles
+                    </Typography>
+                    <Button
+                        variant={"outlined"}
+                        size={"small"}
+                        color={"secondary"}
+                        onClick={() => navigate("/bundles/new")}
+                        sx={{ position: "absolute", right: 0 }}
+                    >
+                        <AddIcon/>
+                        <Typography variant={"button"} sx={{ display: { xs: "none", sm: "flex" } }}>Create bundle</Typography>
+                    </Button>
+                </Box>
+            </Container>
 
             <Grid container spacing={ 2 } >
                 <Grid key={"left-grid"} size="grow" sx={{ display: { xs: "none", md: "flex" }}}/>
