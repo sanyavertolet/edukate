d4b522ee5696c9eec579e09ea60bc88688b8e2a6
sanyavertolet <lxnfrolov@gmail.com>
2025-07-16 00:56:28 +0400

    Add RoleHierarchy support to security configurations
    ### What's done:
     * Added `RoleHierarchyConfiguration` import to `BackendWebSecurityConfig` and `NotifierWebSecurityConfig`.
     * Simplified `RoleHierarchy` configuration in `RoleHierarchyConfiguration` with compact role mapping.
     * Refactored `RoleHierarchyConfiguration` to a pure `@Configuration` class, removing unused beans.

--- edukate-auth/src/main/java/io/github/sanyavertolet/edukate/auth/configs/RoleHierarchyConfiguration.java
+++ edukate-auth/src/main/java/io/github/sanyavertolet/edukate/auth/configs/RoleHierarchyConfiguration.java
@@ -1,27 +1,14 @@
 package io.github.sanyavertolet.edukate.auth.configs;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.web.FilterInvocation;
-import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 public class RoleHierarchyConfiguration {
     @Bean
     public RoleHierarchy roleHierarchy() {
-        return RoleHierarchyImpl.fromHierarchy("""
-                ROLE_ADMIN > ROLE_MODERATOR
-                ROLE_MODERATOR > ROLE_USER
-                ROLE_USER >""");
-    }
-
-    @Bean
-    public SecurityExpressionHandler<FilterInvocation> webExpressionHandler(RoleHierarchy roleHierarchy) {
-        DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
-        expressionHandler.setRoleHierarchy(roleHierarchy);
-        return expressionHandler;
+        return RoleHierarchyImpl.fromHierarchy("ROLE_ADMIN > ROLE_MODERATOR > ROLE_USER");
     }
 }
--- edukate-notifier/src/main/java/io/github/sanyavertolet/edukate/notifier/configs/NotifierWebSecurityConfig.java
+++ edukate-notifier/src/main/java/io/github/sanyavertolet/edukate/notifier/configs/NotifierWebSecurityConfig.java
@@ -1,5 +1,6 @@
 package io.github.sanyavertolet.edukate.notifier.configs;
 
+import io.github.sanyavertolet.edukate.auth.configs.RoleHierarchyConfiguration;
 import io.github.sanyavertolet.edukate.auth.configs.WebSecurityConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -11,5 +12,5 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 @Profile("secure")
-@Import(WebSecurityConfig.class)
+@Import({WebSecurityConfig.class, RoleHierarchyConfiguration.class})
 public class NotifierWebSecurityConfig { }
