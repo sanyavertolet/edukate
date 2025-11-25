0ac09184a6581f25da42ccc10808a5253fcaec4f
sanyavertolet <lxnfrolov@gmail.com>
2025-11-25 22:45:00 +0400

    Add reactive Mongo transaction manager configuration
    ### What's done:
     * Introduced `ReactiveMongoTransactionManager` bean in `MongoConfig` for both `edukate-notifier` and `edukate-backend`.
     * Ensured transactional support for reactive MongoDB operations through Spring configuration.

--- edukate-notifier/src/main/java/io/github/sanyavertolet/edukate/notifier/configs/MongoConfig.java
+++ edukate-notifier/src/main/java/io/github/sanyavertolet/edukate/notifier/configs/MongoConfig.java
@@ -1,8 +1,18 @@
 package io.github.sanyavertolet.edukate.notifier.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.ReactiveMongoTransactionManager;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @Configuration
 @EnableReactiveMongoAuditing
-public class MongoConfig { }
+public class MongoConfig {
+    @Bean
+    public ReactiveMongoTransactionManager reactiveMongoTransactionManager(
+            ReactiveMongoDatabaseFactory factory
+    ) {
+        return new ReactiveMongoTransactionManager(factory);
+    }
+}
