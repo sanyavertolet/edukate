1160baef64d95b040b9f2e2e99bb0835189b406a
sanyavertolet <lxnfrolov@gmail.com>
2026-04-12 02:51:27 +0400

    Add @JsonPropertyOrder to BaseNotificationDto
    ### What's done:
     * Annotated `BaseNotificationDto` with `@JsonPropertyOrder` to ensure a consistent property order in serialization.
     * Specified the order: `createdAt`, `uuid`, `isRead`, and `_type`.

--- edukate-notifier/src/main/kotlin/io/github/sanyavertolet/edukate/notifier/dtos/BaseNotificationDto.kt
+++ edukate-notifier/src/main/kotlin/io/github/sanyavertolet/edukate/notifier/dtos/BaseNotificationDto.kt
@@ -1,10 +1,12 @@
 package io.github.sanyavertolet.edukate.notifier.dtos
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.time.Instant
 
+@JsonPropertyOrder("createdAt", "uuid", "isRead", "_type")
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "_type")
 @JsonSubTypes(
     JsonSubTypes.Type(value = SimpleNotificationDto::class, name = "simple"),
